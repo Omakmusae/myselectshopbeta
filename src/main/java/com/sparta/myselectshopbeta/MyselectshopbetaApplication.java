4faa63599693411891ffd9c0cbb24b6018271eb6
DESKTOP-8BAC7IL\pak92 <pak926@nate.com>
2023-04-19 16:53:42 +0900

    초기 버전 - 네이버 검색 API 연동 - 검색 결과를 목록으로 보여주기

---
+++ src/main/java/com/sparta/myselectshopbeta/MyselectshopbetaApplication.java
@@ -0,0 +1,17 @@
+package com.sparta.myselectshopbeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyselectshopbetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyselectshopbetaApplication.class, args);
+	}
+
+}
+
+
+// 네이버 API 아이디 : t_DJSerfkN_PNSgjyh2f
+// 비번 : BUy5Lfk4qs
